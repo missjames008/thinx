39e3ba51520a256429d53956f7fb4ac63f09d5d3
Jocelyn James <missjames008@gmail.com>
2020-10-29 00:49:08 -0400

    custom dropdown

--- js/index.js
+++ js/index.js
@@ -26,3 +26,24 @@ $(document).ready(function() {
     }
   });
 });
+
+//code for custom dropdown
+//TOGGLING NESTED ul
+$(".drop-down .selected a").click(function() {
+    $(".drop-down .options ul").toggle();
+});
+
+//SELECT OPTIONS AND HIDE OPTION AFTER SELECTION
+$(".drop-down .options ul li a").click(function() {
+    var text = $(this).html();
+    $(".drop-down .selected a span").html(text);
+    $(".drop-down .options ul").hide();
+});
+
+
+//HIDE OPTIONS IF CLICKED ANYWHERE ELSE ON PAGE
+$(document).bind('click', function(e) {
+    var $clicked = $(e.target);
+    if (! $clicked.parents().hasClass("drop-down"))
+        $(".drop-down .options ul").hide();
+});
