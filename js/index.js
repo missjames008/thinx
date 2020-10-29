44b12dcd633b8c9af360db23b3f33dd059dd02c8
Jocelyn James <missjames008@gmail.com>
2020-10-29 02:14:59 -0400

    order functionality

--- js/index.js
+++ js/index.js
@@ -47,3 +47,25 @@ $(document).bind('click', function(e) {
     if (! $clicked.parents().hasClass("drop-down"))
         $(".drop-down .options ul").hide();
 });
+
+//code for custom counter
+$(document).ready(function() {
+  const minus = $('.quantity__minus');
+  const plus = $('.quantity__plus');
+  const input = $('.quantity__input');
+  minus.click(function(e) {
+    e.preventDefault();
+    var value = input.val();
+    if (value > 1) {
+      value--;
+    }
+    input.val(value);
+  });
+
+  plus.click(function(e) {
+    e.preventDefault();
+    var value = input.val();
+    value++;
+    input.val(value);
+  })
+});
